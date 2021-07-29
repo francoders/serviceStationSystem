363169d16d63590988429674e909cec233f12980
Patricio Reyes <reyes.patricio.p@gmail.com>
2021-07-29 19:33:37 -0400

    pequeñas actualizaciones

--- SistemaEstaciones/SQLQuery1.sql
+++ SistemaEstaciones/SQLQuery1.sql
@@ -1,9 +1,13 @@
-﻿CREATE TABLE Estacionservicio (
+﻿CREATE TABLE EstacionServicio (
 IdEstacionServicio VARCHAR (50) PRIMARY KEY,
 Direccion VARCHAR (50) NOT NULL,
 CodRegion VARCHAR (50) NOT NULL,
 CapacidadMax INT NOT NULL,
-HorarioAtencio VARCHAR (50) NOT NULL
+HorarioAtencio VARCHAR (50) NOT NULL,
+
+CONSTRAINT fk_estacionservicio_region FOREIGN KEY(CodRegion)
+REFERENCES Region(CodigoRegion)
+
 );
 
 CREATE TABLE Puntocarga (
@@ -11,4 +15,9 @@ IdPuntoCarga VARCHAR (50) PRIMARY KEY NOT NULL,
 Tipo INT NOT NULL,
 CapacidadMaxVehiculos INT NOT NULL,
 FechaReemplazo VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE Region (
+NombreRegion VARCHAR(50) NOT NULL,
+CodigoRegion VARCHAR(50) PRIMARY KEY NOT NULL
 );
