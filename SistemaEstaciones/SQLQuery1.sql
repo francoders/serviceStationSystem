b24972ef1e6c8f84abc84020b37815bdcea0d714
Patricio Reyes <reyes.patricio.p@gmail.com>
2021-07-29 10:01:16 -0400

    comienzo bd consulta con tablas realizadas

---
+++ SistemaEstaciones/SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE Estacionservicio (
+IdEstacionServicio VARCHAR (50) PRIMARY KEY,
+Direccion VARCHAR (50) NOT NULL,
+CodRegion VARCHAR (50) NOT NULL,
+CapacidadMax INT NOT NULL,
+HorarioAtencio VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE Puntocarga (
+IdPuntoCarga VARCHAR (50) PRIMARY KEY NOT NULL,
+Tipo INT NOT NULL,
+CapacidadMaxVehiculos INT NOT NULL,
+FechaReemplazo VARCHAR (50) NOT NULL
+);
